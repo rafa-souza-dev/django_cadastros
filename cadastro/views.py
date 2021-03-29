39afc286021689b5343f57c8d95dc482724e5648
Your Name <you@example.com>
2021-03-29 18:00:21 -0300

    testando dinamicidade

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Pessoa
 
