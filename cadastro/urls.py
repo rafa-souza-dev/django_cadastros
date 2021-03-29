e551395b2b5258df58e753c888a418ce2bb27bed
Your Name <you@example.com>
2021-03-29 19:22:43 -0300

    criando forma de editar os registros

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from .views import HomeCreate
 from .views import lista
-from .views import pessoa
+from .views import PessoaUpdate
 
 
 urlpatterns = [
     path('', HomeCreate.as_view(), name='HomeCreate'),
     path('lista/', lista, name='lista'),
-    path('pessoa/<int:pessoa_id>', pessoa, name='pessoa'),
+    path('pessoa/<int:pk>', PessoaUpdate.as_view(), name='pessoa'),
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Pessoa
@@ -13,15 +13,14 @@ class HomeCreate(CreateView):
     success_url = reverse_lazy('lista')
 
 
+class PessoaUpdate(UpdateView):
+    model = Pessoa
+    fields = ['nome', 'sobrenome', 'idade', 'data_nascimento', 'email', 'apelido', 'observacao']
+    template_name = 'pessoa.html'
+    success_url = reverse_lazy('lista')
+
+
 def lista(request):
     pessoas = Pessoa.objects.all()
 
     return render(request, 'lista.html', {'pessoas': pessoas})
-
-
-def pessoa(request, pessoa_id):
-    pessoas = Pessoa.objects.all()
-    pessoa = get_object_or_404(Pessoa, id=pessoa_id)
-
-    return render(request, 'pessoa.html', {'pessoas': pessoas,
-                                           'pessoa': pessoa})
