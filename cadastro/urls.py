1b66348f4bd41e66983a102c47c556447c39ca10
Your Name <you@example.com>
2021-03-29 16:17:17 -0300

    criando os links do site

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views import home
 from .views import lista
+from .views import pessoa
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('lista/', lista, name='lista'),
+    path('pessoa/<int:pessoa_id>', pessoa, name='pessoa'),
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Pessoa
 
@@ -6,10 +7,18 @@ from .models import Pessoa
 def home(request):
     pessoas = Pessoa.objects.all()
 
-    return render(request, 'home.html', {'pessoas':pessoas})
+    return render(request, 'home.html', {'pessoas': pessoas})
 
 
 def lista(request):
     pessoas = Pessoa.objects.all()
 
-    return render(request, 'lista.html', {'pessoas':pessoas})
+    return render(request, 'lista.html', {'pessoas': pessoas})
+
+
+def pessoa(request, pessoa_id):
+    pessoas = Pessoa.objects.all()
+    pessoa = get_object_or_404(Pessoa, id=pessoa_id)
+
+    return render(request, 'pessoa.html', {'pessoas': pessoas,
+                                           'pessoa': pessoa})
